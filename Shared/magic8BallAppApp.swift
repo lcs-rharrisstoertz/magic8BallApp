08ba3adcc6c0135b4b2df20b8871a324db59a609
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-09-16 09:41:46 -0400

    Initial Commit

---
+++ Shared/magic8BallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic8BallAppApp.swift
+//  Shared
+//
+//  Created by Harris-Stoertz, Rowan on 2021-09-16.
+//
+
+import SwiftUI
+
+@main
+struct magic8BallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
